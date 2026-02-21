87d58cabf0fba6bf02da427484619cfc311fa27e
sumaiya5896 <sumaiyaaiman5896@gmail.com>
2026-02-21 21:51:23 +0530

    second commit

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI ,Request, Form
 from pydantic import BaseModel
 from google import genai
 import os
 from dotenv import load_dotenv
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 load_dotenv()
-                  
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 app=FastAPI()
 
-class Question(BaseModel):
-    question: str
-@app.get("/")
-def home():
-    return {"message": "Chatbot is running"}
+templates = Jinja2Templates(directory="templates")
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-@app.post("/chat")
-def chat(q: Question):
-    response = client.models.generate_content(
-        model="gemini-3-flash-preview",
-        contents=q.question
-    )
-    return{
-        "question": q.question,
-        "answer": response.text
-    }
+@app.get("/",response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "answer":"", "question": ""})
+
+@app.post("/chat",response_class=HTMLResponse)
+def chat(request:Request, question:str=Form(...)):
+    try:
+        response = client.models.generate_content(
+            model="gemini-3-flash-preview",
+            contents=question
+        )
+        answer = response.text
+
+    except Exception as e:
+        answer = f"Error: {str(e)}"
+        
+    return templates.TemplateResponse("index.html", {"request": request, "answer": answer, "question": question})
+    
+        
